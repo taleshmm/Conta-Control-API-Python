ec30d05c6f9b778924c62969964a295f1a018773
taleshmm <taleshmm@hotmail.com>
2024-03-27 10:41:53 +0000

    feat: project end

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# Python-Backend
-Created apis in python
+## Conta Control API Python
+
+This is an API project built with FastAPI for a system to manage accounts. It includes routes for authentication with encryption, user creation, user permission management (admin, stand, and premium), user management (create, update, get one, get all), and full account management (CRUD).
+
+### Technologies Used
+
+- **SQLAlchemy**
+- **Alembic**
+- **psycopg2_binary**
+- **fastapi**
+- **passlib**
+- **uvicorn**
+- **PyJWT**
+
+### Key Features
+
+- **Secure Authentication:** Implementation of authentication with encryption to ensure user information security.
+- **User Management:** Routes to create, update, get one, get all users in the system.
+- **Account Management:** Routes to create, update, get one, get all accounts for system users.
+- **Permission Management:** Routes to define and verify user permissions (admin, stand, and premium).
+
+### Running the Project
+
+1. Install project dependencies:
+
+   ```
+   pip install -r requirements.txt
+   ```
+
+2. Run the server:
+
+   ```
+   uvicorn main:app --reload
+   ```
+
+3. Access the API documentation at `http://localhost:8000/docs` to view and test available routes.
+
+### How to Contribute
+
+- Fork the project.
+- Create a branch for your feature (`git checkout -b feature/FeatureName`).
+- Commit your changes (`git commit -am 'Adding a new feature'`).
+- Push to the branch (`git push origin feature/FeatureName`).
+- Create a new Pull Request.
+
+```
+
+You can copy and paste this markdown content into your README file.
+```
--- test/test_auth.py
+++ test/test_auth.py
@@ -1,48 +1,76 @@
 from http import HTTPStatus
 from unittest import TestCase, mock
 
+import jwt
 from fastapi.testclient import TestClient
 
-from auth.utils import verify_token
+from auth import schemas
+from auth.utils import ALGORITHM, SECRET_KEY, verify_token
 from database import get_db
 from main import app
 
 
+def override_get_db():...
+
+app.dependency_overrides[get_db] = override_get_db
+client = TestClient(app)
 class AuthTestCase(TestCase):
     def setUp(self):
-        ...
+        self.user_auth = {
+            "email": "john@doe.com",
+            "password": "12345"           
+        }
+        self.user_authentication = schemas.UserAuthentication(**self.user_auth)
+        self.token = jwt.encode({"email": "john@doe.com"}, SECRET_KEY, algorithm=ALGORITHM)
+        self.user_data = {
+            "id": 1,
+            "email": "john@doe.com",
+            "name": "John Doe",
+            "nickname": "JD",
+            "sex": "M",
+            "type_access": 2
+        }
+        self.user_auth_sing = {
+            "email": "john@doe.com",
+            "password": "12345",
+            "confirm_password": "12345",
+            "name": "John Doe",
+            "nickname": "JD",
+            "sex": "M",
+            "type_access": 2
+        }
         
-    # @mock.patch("users.crud.create_user")
-    # def test_create_new_permission(self, mock_create_user):
-    #     """
-    #       Test create new user must return success
-    #     """
-    #     request = self.user_request
-    #     mock_create_user.return_value = request
-    #     app.dependency_overrides[verify_token] = self.override_verify_token
-    #     response = client.post("/user", json=request)
-        
-    #     user_response = CreateUserResponse(**request)
-        
-    #     assert response.status_code == HTTPStatus.CREATED
-    #     assert response.json() == user_response.__dict__.copy()
-     
-    # @mock.patch("users.crud.get_user_email")   
-    # @mock.patch("users.crud.create_user")
-    # def test_create_permission_but_exist(self, mock_create_user, mock_get_user_email):
-    #     """
-    #       Test create user but user exist return error
-    #     """
-    #     request = self.user_request
-    #     mock_get_user_email.return_value = self.user_instance
-    #     error_message = f"User already exists email: {request['email']}"
-    #     mock_create_user.side_effect = HTTPException(
-    #     status_code=HTTPStatus.BAD_REQUEST,
-    #      detail=error_message,
-    #  )
-       
-    #     response = client.post(f"/user", json=request)
+    @mock.patch("auth.utils.validate_user")
+    @mock.patch("auth.utils.jwt.encode")
+    def test_create_token(self, mock_encode, mock_validate_user):
+        mock_validate_user.return_value = self.user_data
+        mock_encode.return_value = "mocked_token"
 
-    #     assert response.status_code == HTTPStatus.BAD_REQUEST
-    #     assert response.json() == {"detail": error_message}
-    
+        response = client.post("/auth/login", json=self.user_auth)
+        data = response.json()
+
+        assert response.status_code == HTTPStatus.OK
+        assert "token" in data
+        assert "exp" in data
+
+        mock_encode.assert_called_once_with(
+            {"email": self.user_auth["email"], "exp": mock.ANY},
+            SECRET_KEY,
+            ALGORITHM
+        )
+        
+    @mock.patch("auth.utils.create_token")
+    @mock.patch("auth.crud.create_user")
+    def test_signup(self, mock_create_user, mock_create_token):
+        mock_create_user.return_value = self.user_data
+        mock_create_token.return_value = "mocked_token"
+
+        response = client.post("/auth/signup", json=self.user_auth_sing)
+        data = response.json()
+
+        assert response.status_code == HTTPStatus.OK
+        assert "user" in data
+        assert data["user"] == self.user_data
+        assert "token" in data
+        assert data["token"] == "mocked_token"
+        
